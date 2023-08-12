168570d0568fd137d44d4f820920a7a48bb2d501
seanluse41 <seanluse41@gmail.com>
2023-08-12 17:03:28 +0900

    add textbox endtext marker

--- singletons/textBox.gd
+++ singletons/textBox.gd
@@ -1,14 +1,19 @@
 extends Control
-@onready var label = $MarginContainer/Panel/MarginContainer/Label
+
+@onready var label = $MarginContainer/Panel/MarginContainer/HBoxContainer/label
+@onready var next_line = $MarginContainer/Panel/MarginContainer/HBoxContainer/nextLine
+@onready var next_line_timer = $nextLineTimer
 
 @onready var text: String
 var isAnimating: bool = false
+var isLastText: bool = true
 var tween
 
 func _ready():
 	Signals.textStarted.connect(_addText)
 	Signals.textSkip.connect(_handleSkipText)
 	Signals.textClose.connect(_closeTextBox)
+	next_line_timer.one_shot = false
 
 func _process(_delta):
 	if label.visible_ratio < 1.0:
@@ -18,7 +23,11 @@ func _process(_delta):
 
 func _openTextBox():
 	self.show()
-	
+
+func _closeTextBox():
+	self.hide()
+	label.visible_ratio = 0.0
+
 func _addText(newText):
 	_openTextBox()
 	label.text = newText
@@ -32,14 +41,20 @@ func _animateText():
 
 func _effectCompleted():
 	isAnimating = false
+	next_line.show()
+	next_line_timer.start(0.8)
 	Signals.textFinished.emit()
-	
-func _closeTextBox():
-	self.hide()
-	label.visible_ratio = 0.0
 
 func _handleSkipText():
 	if isAnimating:
 		tween.set_speed_scale(10.0)
 	else:
 		_closeTextBox()
+
+func _toggleNextLineShow():
+	print("toggle")
+	next_line.visible = !next_line.visible
+
+func _on_next_line_timer_timeout():
+	print("timeout")
+	_toggleNextLineShow()
