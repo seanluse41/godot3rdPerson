7e099f2e88d4aa59f14ea6981a173d2c94f8ac0d
seanluse41 <seanluse41@gmail.com>
2023-08-23 00:02:22 +0900

    more simple state machine for now

--- scripts/robotPlayer.gd
+++ scripts/robotPlayer.gd
@@ -6,17 +6,12 @@ class_name Player
 @onready var camera_mount = $CameraMount
 @onready var interact_timer = $interactBox/interactTimer
 @onready var animation_player = %AnimationPlayer
-@onready var fsm = $FiniteStateMachine as FiniteStateMachine
-@onready var player_idle = $FiniteStateMachine/playerIdle as playerIdle
-@onready var player_walk = $FiniteStateMachine/playerWalk as playerWalk
-@onready var player_kick = $FiniteStateMachine/playerKick as playerKick
-@onready var player_jump = $FiniteStateMachine/playerJump as playerJump
-@onready var player_run = $FiniteStateMachine/playerRun as playerRun
 
 @export var runningSpeed = 10.0
 @export var SENSITIVITY_X = 0.4
 @export var SENSITIVITY_Y = 0.01
-@export var currentState : State
+@export var currentState : int = States.IDLE
+enum States {IDLE, WALK, RUN, JUMP, INTERACT, SEARCH, LOCKED}
 
 var SPEED = 3.0
 const JUMP_VELOCITY = 4.5
@@ -36,20 +31,22 @@ func _ready():
 	Signals.canNotInteract.connect(_handleCanNotInteractBox)
 	Signals.leftInteractArea.connect(_handleLeftInteractArea)
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
-	player_walk.walking.connect(fsm.changeState.bind(currentState))
+
+func _changeState(newState):
+	currentState = newState
 
 func _input(event):
 	if event is InputEventMouseMotion:
 		twistInput = - event.relative.x * SENSITIVITY_X
-		pitchInput = event.relative.y * SENSITIVITY_Y
+		pitchInput = -event.relative.y * SENSITIVITY_Y
 		rotate_y(deg_to_rad(twistInput))
 		visuals.rotate_y(deg_to_rad(-twistInput))
-		$CameraMount.rotate_x(pitchInput)
-		$CameraMount.rotation.x = clamp($CameraMount.rotation.x, -0.5, 0.5)
+		camera_mount.rotate_x(pitchInput)
+		camera_mount.rotation.x = clamp(camera_mount.rotation.x, -0.6, 0)
+
 func _handleSearch():
 	if is_on_floor():
 		if not isLocked:
-			animation_player.play("T-pose")
 			isLocked = true
 			_unlockCharacter()
 
@@ -61,58 +58,67 @@ func _handleInteract():
 
 func _handleRun():
 	SPEED = runningSpeed
-	isRunning = true
 
 func _handleJump():
 	velocity.y = JUMP_VELOCITY
 
+func chooseAnimation():
+	match currentState:
+		States.IDLE:
+			animation_player.play("Idle")
+		States.WALK:
+			animation_player.play("Run")
+		States.RUN:
+			animation_player.play("Sprint")
+		States.JUMP:
+			animation_player.play("Jump")
+		States.SEARCH:
+			animation_player.play("T-pose")
+		States.INTERACT:
+			animation_player.play("Idle")
+		States.LOCKED:
+			animation_player.play("Idle")
+		_:
+			animation_player.play("Idle")
+
 func _physics_process(delta):
+	chooseAnimation()
 	if Input.is_action_pressed("search"):
+		_changeState(States.SEARCH)
 		_handleSearch()
 	if Input.is_action_just_pressed("interact"):
+		_changeState(States.INTERACT)
 		_handleInteract()
 	if Input.is_action_pressed("run"):
+		_changeState(States.RUN)
 		_handleRun()
-	else:
-		SPEED = walkingSpeed
-		isRunning = false
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y -= gravity * delta
-	
 	if Input.is_action_just_pressed("quit"):
 		get_tree().quit()
 
 	# Handle Jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor() and not isLocked:
+	if Input.is_action_just_pressed("jump") and is_on_floor() and currentState != States.LOCKED:
+		_changeState(States.JUMP)
 		_handleJump()
 
 	var input_dir = Input.get_vector("left", "right", "forward", "backward")
 	var direction = (transform.basis * Vector3( -input_dir.x, 0, -input_dir.y)).normalized()
 	if direction:
 		visuals.look_at(direction)
-		if !isLocked:
-			if isRunning:
-				if animation_player.current_animation != "running":
-					animation_player.play("Sprint")
-			else:
-				if animation_player.current_animation != "walking":
-					animation_player.play("Run")
-			visuals.look_at(position + direction)
-			
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
+		if currentState != States.LOCKED:
+			visuals.look_at(position - direction)
+		velocity.x = -direction.x * SPEED
+		velocity.z = -direction.z * SPEED
 	else:
-		if !isLocked:
-			if animation_player.current_animation != "Idle":
-				animation_player.play("Idle")
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
 	if !isLocked:
 		move_and_slide()
 
 func _unlockCharacter():
-	isLocked = false
+	_changeState(States.IDLE)
 
 func _handleCanInteractBox(node: Node):
 	currentInteractable = node
@@ -141,7 +147,7 @@ func interact():
 		pass
 	else:
 		if currentInteractable.has_method("onInteract"):
-			isLocked = true
+			_changeState(States.LOCKED)
 			currentInteractable.onInteract()
 			interact_box.hide()
 			interact_timer.start()
--- scripts/states/player/playerIdle.gd
+++
@@ -1,8 +0,0 @@
-extends State
-
-class_name playerIdle
-
-@onready var animation_player = %AnimationPlayer
-
-func idle():
-	animation_player.play("Idle")
